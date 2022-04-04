f5b68465587eed034c2629cba0e3d140a35e53cc
siyadpk <123siyad007@gmail.com>
2022-04-05 01:46:14 +0530

    login completed using $event binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  name='hello world'
+  accno='password please!!!'
+  acc=''
+  pwd=''
+
+  database:any = {
+    1000:{usname:'siya',passwd:123,bal:1000},
+    1001:{usname:'zia',passwd:113,bal:1000},
+    1002:{usname:'ziyad',passwd:223,bal:1000}
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  acntTake(event:any){
+    this.acc=event.target.value
+    console.log(this.acc);
+    
+  }
+
+  paswdTake(event:any){
+    this.pwd=event.target.value
+    console.log(this.pwd);
+    
+  }
+
+
+  login(){
+    var acc=this.acc
+    var pwd=this.pwd
+
+    let database=this.database
+    console.log(database);
+    
+
+    if(acc in database){
+
+      if(pwd == database[acc]['passwd']){
+        alert("login success")
+      }
+      else{
+        alert("incorrect password")
+      }
+
+    }
+    else{
+      alert("user doesnot exist")
+    }
   }
 
 }
